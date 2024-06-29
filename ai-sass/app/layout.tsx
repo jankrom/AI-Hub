d23e65ab0e20f08437a0ce103dbe3c6f296fb7f0
Justin Ankrom <justinankrom1@gmail.com>
2024-06-29 19:14:18 -0400

    Complete dashboard page

--- ai-sass/app/layout.tsx
+++ ai-sass/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "AI-Hub",
   description: "AI Generation Platform",
+  
 };
 
 export default function RootLayout({
--- ai-sass/components/mobile-sidebar.tsx
+++ ai-sass/components/mobile-sidebar.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import Sidebar from "./sidebar"
+import { useEffect, useState } from "react"
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden hover:bg-accent hover:text-accent-foreground">
-        <Menu />
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <Sidebar />
